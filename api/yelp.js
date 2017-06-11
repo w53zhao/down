2f79a53e93d6ab1cc820acf5ff3ccaaead2040a4
Wenxi Zhao <w53zhao@uwaterloo.ca>
2017-06-11 12:48:48 -0400

    calculate midpoint

--- api/yelp.js
+++ api/yelp.js
@@ -3,13 +3,21 @@ var yelp = new Yelp({
     id: 'Fhpp8i_pXmQ_rszyX3ru1Q',
     secret: 'DZgzafJq7iAV5NnTMZCMXTzXNsU47U2RRR8OOsd0OhMnrF2awTfKwnh36TndGyb9'
 });
+var geolib = require('geolib');
 
 module.exports = {
-    search: function(venue, latitude, longitude) {
+    search: function(event, venue, latitude, longitude) {
+        // query db with event to get sender coordinates
+        var locations = [
+            {latitude: 43.4717443, longitude: -80.5475369},
+            {latitude: latitude, longitude: longitude}
+        ];
+        var center = geolib.getCenter(locations);
+
         return yelp.search(
             "categories=" + venue +
-            "&latitude=" + latitude +
-            "&longitude=" + longitude +
+            "&latitude=" + center.latitude +
+            "&longitude=" + center.longitude +
             "&limit=5" +
             "&sort_by=distance"
         );
--- index.js
+++ index.js
@@ -3,25 +3,18 @@ var app = express();
 var yelp = require('./api/yelp');
 var Venue = require('./venue');
 
-//app.set('port', (process.env.PORT || 5000));
-
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
 app.get('/venues', function(req, res) {
     var query = req.query;
+    var event = query.event || 0;
     var venue = query.venue;
     var latitude = query.latitude;
     var longitude = query.longitude;
 
-    yelp.search(venue, latitude, longitude)
+    yelp.search(event, venue, latitude, longitude)
         .then(function(results) {
             var venues = [];
             for (var i = 0; i < results.businesses.length; i++) {
